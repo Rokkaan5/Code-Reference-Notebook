be1398053a021b67ccfbff35692a2a9d1e325bda
Jasmine Kobayashi <rokkakobayashi@msn.com>
2023-10-13 23:40:49 -0600

    add feed forward code

--- CU-Boulder/NeuralNetworks/HW/hw2/Part1/A2p1.py
+++ CU-Boulder/NeuralNetworks/HW/hw2/Part1/A2p1.py
@@ -1,18 +1,3 @@
-""" 
-Code logistic regression using Python. DO NOT use a "package" but rather, code it by hand. Yes you can use simple packages like Numpy, etc. You cannot use a Logistic regression package. 
-
-You must write code that will:
-(a) Read in any .csv labeled dataset where the Label is called "LABEL". 
-(b) Your code should create "y" (the label) as a numpy array of 0 and 1. So, if the labels are originally something like POS and NEG, you r code will need to determine this, and then update all POS to 1 and all NEG to 0. Your code should be able to do this for any two label options. 
-(c) Your code will also create X which is the entire dataset (without the labels) as a numpy array. 
-(d) Your code will initialize w and b and a learning rate. 
-(e) Your code will need to contain a function for Sigmoid and for z, etc. 
-(f) Your code will then need to perform gradient descent to optimize (reduce) LCE.
-(g) Be sure that your code can run iteratively for as many epochs as you wish. 
-(h) Show a final confusion matrix (see example below) AND show a graph of how the LCE reduces as epochs iterate (see example below)
-
-Recommended: Use your code to see if your by-hand results from above are right. The goal here is to really understand all the steps, which steps are repeated (epochs), the loss function (LCE here), the activation function (Sigmoid here), updating the parameters, etc.  
-"""
 #%% libraries
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,18 +6,17 @@ import math
 from sklearn.preprocessing import LabelEncoder,MinMaxScaler
 from sklearn.metrics import confusion_matrix  
 import seaborn as sns
-
+import random
 
 # %%
 class OneLayer_OneOutput_FF():
     def __init__(self,
                  data_filename = "A2_Data_JasmineKobayashi.csv", 
-                 hidden_units = 4, 
-                 activation="sigmoid",
                  scale_data = True,
-                 weight_init = "simple"):
+                 verbose = True):
+        # Read Data
         self.df = pd.read_csv(data_filename)
-
+        
         # create X and y, assuming data has column "LABEL" as column with labels
         ## X matrix
         if scale_data:
@@ -41,55 +25,105 @@ class OneLayer_OneOutput_FF():
         else:
             X = np.array(self.df.drop(columns=['LABEL'],axis=1))
         self.X = X
+
         ## y
-        self.y = df[['LABEL']]
+        self.y = np.array(self.df[['LABEL']])
 
-        # TODO: create W1
-        if weight_init == "random":
-            pass
-        else:
-            # self.W1 = np.array()
-            pass
-        # TODO: create H1
+        if verbose:
+            print("Shape of X:", self.X.shape)
+            print("X matrix: \n", self.X)
+            print("Shape of y:", self.y.shape)
+            print("y vector: \n", self.y)
 
-        # Other useful stuff-----------------------------------------------------------------------------------
-        # self.epochs = epochs
-        # self.LR = LR
-        # self.y_hat = None
-        # self.Lce_list = []
-        
-        pass
-
-    def MSE_loss(self):
+    def MSE_loss(self,y_hat,y):
         # L = mean((y_hat - y)^2)
-        pass
+        return np.mean((y_hat - y)**2)
         
-    def lin_eq(self,X,w,b):
-        return X @ w + b
+    def lin_eq(self,X,W,B):
+        assert X.shape[1] == W.shape[0], "Linear eq: z = X @ W + B. Shape of X: {}; Shape of W: {}".format(X.shape,W.shape)
+        assert B.shape[0] == X.shape[0], "Linear eq: z = X @ W + B. Length of B should match X rows; Length B = {}; Shape of X = {}".format(B.shape[0],X.shape)
+        return X @ W + B
     
-    def Sigmoid(self,z,derivative = False):
+    def Sigmoid(self,z,
+                derivative = False):
         if derivative:
-            return self.Simoid(z) * (1 - self.Sigmoid(z)) # dS/dz = S(z)(1-S(z))
+            return self.Sigmoid(z) * (1 - self.Sigmoid(z)) # dS/dz = S(z)(1-S(z))
         # Sigmoid
         return 1/(1 + math.e**(-z)) # S(z) = 1/(1 + e^(-z))
-
-
-    # Gradient descent----------------------------------------------requirement (f)
-    def grad_desc(self,y_hat,y,X):
-        error = y_hat-y
-        m = self.n_obs
-        
-        dL_dw = (1/m)*(error.T@X)
-        dL_db = (1/m)*error
-        dL_db = np.mean(dL_db)
-        return dL_dw.T, dL_db
     
-    def LCE(self,y_hat,y):
-        m = self.n_obs
-        summation = np.sum(y*np.log(y_hat)+((1-y)*np.log(1-y_hat)))
-        return (-1/m)*summation
+    def FeedForward(self,
+                    hidden_units= 4,
+                    randomize_parameters = False,
+                    activation = "sigmoid",
+                    verbose = True):
+        # Shape of W1
+        w1_ncol = hidden_units        # Number of W1 cols should be number of hidden units in H1
+        w1_nrow = self.X.shape[1]     # Number of W1 rows should be number of X columns (dim of X)
+        # Shape of B (column vector)
+        b_length = self.X.shape[0]    # length of X rows
+        # Shape of W2:
+        w2_ncol = 1                   # Number of W2 cols should be number of outputs (in this case, only one output)
+        w2_nrow = hidden_units        # Number of W2 rows should be number of hidden units in H1
+        # Shape of C (column vector)
+        c_length = self.X.shape[0]    # length of X rows
+        
+        if randomize_parameters: 
+            # TODO: Finish potential code of randomized parameters
+            # # bounds of randomized weights
+            # bound = 1/np.sqrt(w1_nrow)   # I found online that supposedly this is a standard range for randomized weights
+            # rand_w = [np.random.uniform(-bound,bound) for i in range(w1_ncol)]
+            # self.W1 = np.array([rand_w]*w1_nrow)
 
-    # Visualizations (confusion matrix & Lce plot)==================requirement (h)
+            # self.W2 = np.array([]*w2_nrow)
+            # print('All parameters were randomized')
+            pass
+        else:   # These are the parameters defined by the question 2 in Assignment2
+            self.W1 = np.array([[1]*w1_ncol]*w1_nrow) # otherwise, all weights of W1 = 1
+            self.B = np.array([[0]]*b_length)         # otherwise, all bias (B) = 0
+            self.W2 = np.array([[2]*w2_ncol]*w2_nrow) # otherwise, all weights of W2 = 2
+            self.C = np.array([[0]]*c_length)
+            print("Parameters built for simple calculation (not randomized)")
+        
+        if verbose:
+            # print("# of X cols (should be # of W1 rows):", self.X.shape[1])
+            # print("# of hidden units (should be # of W1 cols):", hidden_units)
+            print("W1 has shape:", self.W1.shape)
+            print("W1 matrix: \n", self.W1)
+            print("Shape of B:", self.B.shape)
+            print("B (bias vector for Z1): \n", self.B)
+            print("Shape of W2:",self.W2.shape)
+            print("W2 matrix: \n", self.W2)
+            print("Shape of C :", self.C.shape)
+            print("C (bias vector for Z2): \n",self.C)
+
+        # Create H1
+        # Z1 = X @ W1 + B
+        self.Z1 = self.lin_eq(self.X,self.W1,self.B)
+        if verbose:
+            print("Shape of Z1:",self.Z1.shape)
+            print("Z1 matrix: \n",self.Z1)
+        self.H1 = self.Sigmoid(self.Z1)
+        # TODO: Add flexibility with other activation functions
+        if verbose: 
+            print("Activation function:",activation)
+            print("H1 matrix: \n", self.H1)
+
+        # Z2 and y-hat
+        # Z2 = H1 @ W2 + C
+        self.Z2 = self.lin_eq(self.H1,self.W2,self.C)
+        self.y_hat = self.Sigmoid(self.Z2)
+
+        if verbose:
+            print("Shape of Z2:", self.Z2.shape)
+            print("Z2 matrix: \n", self.Z2)
+            print("y_hat:\n",self.y_hat)
+        
+    
+    # Gradient descent----------------------------------------------
+    def grad_desc(self,y_hat,y,X):
+        pass
+
+    # Visualizations (confusion matrix & Lce plot)==================
     # Confusion matrix-------------------------------------------------------------
     def ConfusionMatrix(self, y_hat,y):
         self.prediction = y_hat
@@ -157,3 +191,4 @@ class OneLayer_OneOutput_FF():
         # Visualizations
         self.ConfusionMatrix(y_hat=self.y_hat,y=self.y)
         self.plot_LCE()
+# %%
